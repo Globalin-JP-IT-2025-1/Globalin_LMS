c1d53c3f02380a274eac72becb12e42d451f63a4
f0shin <13ro921@naver.com>
2025-06-19 10:02:20 +0900

    update 0619

--- src/main/java/com/library/controller/article/PublicArticleController.java
+++ src/main/java/com/library/controller/article/PublicArticleController.java
@@ -1,7 +1,8 @@
 package com.library.controller.article;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.library.model.Article;
 import com.library.model.PageInfo;
 import com.library.service.ArticleService;
+import com.library.service.MemberService;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,122 +25,142 @@ import lombok.extern.slf4j.Slf4j;
 @AllArgsConstructor
 public class PublicArticleController {
     private final ArticleService articleService; // 게시글
+    private final MemberService memberService; // 작성자
     private PageInfo pageInfo;
     
     public void setPageInfo(Model model) {
-    	model.addAttribute("pageTitleCode", pageInfo.getPageTitleCode());
-    	model.addAttribute("pagePath", pageInfo.getPagePath());
+       model.addAttribute("pageTitleCode", pageInfo.getPageTitleCode());
+       model.addAttribute("pagePath", pageInfo.getPagePath());
     }
     
     // not 목록 조회 --> ok
     @GetMapping("/not")
-    public String getListNot(Model model) {
-		
-		Map<String, Object> articleListWithAuthor = articleService.getAllArticlesByCategoryWithAuthor("not");
-		
-		model.addAttribute("articleList", articleListWithAuthor.get("articleList"));
-		model.addAttribute("authorList", articleListWithAuthor.get("authorList"));
-    	
-    	pageInfo = PageInfo.builder()
-    			.pageTitleCode("21")
-    			.pagePath("page/3-article/articleList_not.jsp")
-    			.build();
-        	
+    public String getListNot(HttpServletRequest request, Model model) {
+      log.info("### {} - {} - {} 요청 매핑 정상 처리!", 
+            this.getClass().getSimpleName(), 
+            request.getRequestURI(),
+            request.getMethod());
+      
+      Map<String, Object> articleListWithAuthor = articleService.getAllArticlesByCategoryWithAuthor("not");
+      
+      model.addAttribute("articleList", articleListWithAuthor.get("articleList"));
+      model.addAttribute("authorList", articleListWithAuthor.get("authorList"));
+       
+       pageInfo = PageInfo.builder()
+             .pageTitleCode("21")
+             .pagePath("page/3-article/articleList_not.jsp")
+             .build();
+           
         setPageInfo(model);
-    	
+       
         return "layout";
     }
     
     // faq 목록 조회
     @GetMapping("/faq")
-    public String getListFaq(Model model) {
-		
-		List<Article> articleList = articleService.getAllArticlesByCategory("faq");
-		model.addAttribute("articleList", articleList);
-    	
-    	pageInfo = PageInfo.builder()
-    			.pageTitleCode("22")
-    			.pagePath("page/3-article/articleList_faq.jsp")
-    			.build();
-        	
+    public String getListFaq(HttpServletRequest request, Model model) {
+      log.info("### {} - {} - {} 요청 매핑 정상 처리!", 
+            this.getClass().getSimpleName(), 
+            request.getRequestURI(),
+            request.getMethod());
+      
+      List<Article> articleList = articleService.getAllArticlesByCategory("faq");
+      model.addAttribute("articleList", articleList);
+       
+       pageInfo = PageInfo.builder()
+             .pageTitleCode("22")
+             .pagePath("page/3-article/articleList_faq.jsp")
+             .build();
+           
         setPageInfo(model);
-    	
+       
         return "layout";
     }
     
     // qna 목록 조회
     @GetMapping("/qna")
-    public String getListQna(Model model) {
-    	
-    	Map<String, Object> articleListWithAuthor = articleService.getAllArticlesByCategoryWithAuthor("qna");
-    	
-    	model.addAttribute("articleList", articleListWithAuthor.get("articleList"));
-    	model.addAttribute("authorList", articleListWithAuthor.get("authorList"));
-    	
-    	pageInfo = PageInfo.builder()
-    			.pageTitleCode("23")
-    			.pagePath("page/3-article/articleList_qna.jsp")
-    			.build();
-    	
-    	setPageInfo(model);
-    	
-    	return "layout";
+    public String getListQna(HttpServletRequest request, Model model) {
+       log.info("### {} - {} - {} 요청 매핑 정상 처리!", 
+             this.getClass().getSimpleName(), 
+             request.getRequestURI(),
+             request.getMethod());
+       
+       Map<String, Object> articleListWithAuthor = articleService.getAllArticlesByCategoryWithAuthor("qna");
+       
+       model.addAttribute("articleList", articleListWithAuthor.get("articleList"));
+       model.addAttribute("authorList", articleListWithAuthor.get("authorList"));
+       
+       pageInfo = PageInfo.builder()
+             .pageTitleCode("23")
+             .pagePath("page/3-article/articleList_qna.jsp")
+             .build();
+       
+       setPageInfo(model);
+       
+       return "layout";
     }
     
     // not 상세 조회 --> test 중
     @GetMapping("/not/{articlesId}")
-    public String getDetailNot(@PathVariable("articlesId") int articlesId, 
-			    		       Model model) {
-		
-		try {
-			Map<String, Object> articleWithAuthorAndReplies = articleService.getArticleWithAuthorAndReplies(articlesId);
-			model.addAttribute("article", articleWithAuthorAndReplies.get("article")); // 게시글 상세 정보
-			model.addAttribute("a_author", articleWithAuthorAndReplies.get("a_author")); // 작성자 정보
-			model.addAttribute("replyList", articleWithAuthorAndReplies.get("replyList")); // 댓글 리스트
-			model.addAttribute("r_authorList", articleWithAuthorAndReplies.get("r_authorList")); // 댓글 작성자 리스트
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-			
-			model.addAttribute("alertType", "fail");
-			model.addAttribute("alertMesssage", "[공지사항] 게시글 상세 조회 실패");
-		}
-		
-    	pageInfo = PageInfo.builder()
-    			.pageTitleCode("21")
-    			.pagePath("page/3-article/articleDetail_not.jsp")
-    			.build();
-        	
+    public String getDetailNot(@PathVariable("articlesId") int articlesId, HttpServletRequest request, Model model) {
+      log.info("### {} - {} - {} 요청 매핑 정상 처리!", 
+            this.getClass().getSimpleName(), 
+            request.getRequestURI(),
+            request.getMethod());
+      
+      try {
+         Map<String, Object> articleWithAuthorAndReplies = articleService.getArticleWithAuthorAndReplies(articlesId);
+         model.addAttribute("article", articleWithAuthorAndReplies.get("article")); // 게시글 상세 정보
+         model.addAttribute("a_author", articleWithAuthorAndReplies.get("a_author")); // 작성자 정보
+         model.addAttribute("replyList", articleWithAuthorAndReplies.get("replyList")); // 댓글 리스트
+         model.addAttribute("r_authorList", articleWithAuthorAndReplies.get("r_authorList")); // 댓글 작성자 리스트
+         
+      } catch (Exception e) {
+         e.printStackTrace();
+         
+         model.addAttribute("alertType", "fail");
+         model.addAttribute("alertMesssage", "게시글 조회를 실패 하였습니다. 다시 시도해주세요.");
+      }
+      
+       pageInfo = PageInfo.builder()
+             .pageTitleCode("21")
+             .pagePath("page/3-article/articleDetail_not.jsp")
+             .build();
+           
         setPageInfo(model);
-    	
+       
         return "layout";
     }
     
     // qna 상세 조회 (+댓글)
     @GetMapping("/qna/{articlesId}")
-    public String getDetailQna(@PathVariable("articlesId") int articlesId, 
-				    		   Model model) {
-		
-		try {
-			Map<String, Object> articleWithAuthorAndReplies = articleService.getArticleWithAuthorAndReplies(articlesId);
-			model.addAttribute("article", articleWithAuthorAndReplies.get("article")); // 게시글 상세 정보
-			model.addAttribute("author", articleWithAuthorAndReplies.get("author")); // 작성자 정보
-			model.addAttribute("replyList", articleWithAuthorAndReplies.get("replyList")); // 댓글 리스트
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-			
-			model.addAttribute("alertType", "fail");
-			model.addAttribute("alertMesssage", "[Q&A] 게시글 상세 조회 실패");
-		}
-    	
-    	pageInfo = PageInfo.builder()
-    			.pageTitleCode("23")
-    			.pagePath("page/3-article/articleDetail_qna.jsp")
-    			.build();
-        	
+    public String getDetailQna(@PathVariable("articlesId") int articlesId, HttpServletRequest request, Model model) {
+      log.info("### {} - {} - {} 요청 매핑 정상 처리!", 
+            this.getClass().getSimpleName(), 
+            request.getRequestURI(),
+            request.getMethod());
+      
+      try {
+         Map<String, Object> articleWithAuthorAndReplies = articleService.getArticleWithAuthorAndReplies(articlesId);
+         model.addAttribute("article", articleWithAuthorAndReplies.get("article")); // 게시글 상세 정보
+         model.addAttribute("a_author", articleWithAuthorAndReplies.get("a_author")); // 작성자 정보
+         model.addAttribute("replyList", articleWithAuthorAndReplies.get("replyList")); // 댓글 리스트
+         model.addAttribute("r_authorList", articleWithAuthorAndReplies.get("r_authorList")); // 댓글 작성자 리스트
+
+      } catch (Exception e) {
+         e.printStackTrace();
+         
+         model.addAttribute("alertType", "fail");
+         model.addAttribute("alertMesssage", "게시글 조회를 실패 하였습니다. 다시 시도해주세요.");
+      }
+       
+       pageInfo = PageInfo.builder()
+             .pageTitleCode("23")
+             .pagePath("page/3-article/articleDetail_qna.jsp")
+             .build();
+           
         setPageInfo(model);
-    	
+       
         return "layout";
     }
     
